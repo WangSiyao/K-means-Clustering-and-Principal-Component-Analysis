5fc982785934508e475771d9610716355c00df64
Wang Siyao <1521925222@qq.com>
2016-05-02 01:18:24 +0800

    Means

--- findClosestCentroids.m
+++ findClosestCentroids.m
@@ -20,11 +20,9 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
-
-
+    for i=1:size(X,1)
+        [~,idx(i)] = min(sum((repmat(X(i,:),[K,1]) - centroids).^2,2));
+    end
 
 
 % =============================================================
